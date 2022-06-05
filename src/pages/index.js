5dba4121258ec66b64f33be84e2c38bfeb5ca360
spaqin <mw@m-labs.hk>
2022-06-05 21:51:24 +0800

    update the front page a little

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,9 @@ const Index = () => {
       <Metadata title="Home" description="the homepage"/>
       <h1>Home page</h1>
       <h2>Hello there.</h2>
-      <p>There isn't much here at the moment, but maybe it will grow with time :^)</p>
-      <p>Besides few basic things, there should be a gallery of the photos I've taken over the years. If it's not there... well, it will be.</p>
+      <p>Welcome to my world. My world is quite small as I don't try to focus on external issues, just... report what I see.</p>
+      <p>If you're a photography nerd, you may enjoy my blog and gallery.</p>
+      <p>Have fun!</p>
     </Layout>
   )
 }
