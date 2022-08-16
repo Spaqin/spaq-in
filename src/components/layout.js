cb1a7683ef606aae08e96c8f11956e3dbf716474
spaqin <mw@m-labs.hk>
2022-08-16 14:39:49 +0800

    remove debug message

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,6 @@ import * as layoutStyles from "./layout.module.scss"
 const Layout = props => {
   return (
     <div className={layoutStyles.container}>
-      <p> dupa </p>
       <div className={layoutStyles.content}>
         <Header />
         <div className={layoutStyles.mainContent}>{props.children}</div>
