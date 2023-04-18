16fcb337cfbcd0720acc6ad2cb3c7b243752cdc7
mwojcik <mw@m-labs.hk>
2023-04-18 15:07:16 +0800

    linkimagestooriginal

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,7 +97,7 @@ module.exports = {
             resolve: "gatsby-remark-images",
             options: {
               maxWidth: 900,
-              linkImagesToOriginal: false,
+              linkImagesToOriginal: true,
             },
           },
           {
