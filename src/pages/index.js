8597bfe0e5acf82fd22957792ce657c13b7e4feb
spaqin <mw@m-labs.hk>
2026-01-03 16:43:49 +0800

    update footer year, front page intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,14 @@ const Index = () => {
   return (
     <Layout>
       <h1>Home page</h1>
-      <h2>Hello there.</h2>
-      <p>Welcome to my world. My world is quite small as I don't try to focus on external issues, just... report what I see.</p>
-      <p>If you're a photography nerd, you may enjoy my blog and gallery.</p>
-      <p>Have fun!</p>
+      <h2>Hi!</h2>
+      <p>I'm Spaqin, or rather, that's my main internet handle, and this is my personal site (duh).</p>
+      <p>I post mainly about photography, old gear and travel, and I sincerely hope you will find something helpful or interesting here.</p>
+      <p>
+        I like to take pictures, fly FPV and to write in my spare time, but I'm not a professional (albeit I do offer my services); by day I work as a software enginerd.
+        If you're a recruiter or a fellow tech nerd checking my site, see it more as a showcase of somehow managing to wrangle webdev.
+      </p>
+      <p>If you have any questions, please feel free to contact me. And of course, have a lovely day and have fun!</p>
     </Layout>
   )
 }
